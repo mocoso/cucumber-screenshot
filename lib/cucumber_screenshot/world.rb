e5937219d00eadcbcad9b36923263280ef46a215
Joel Chippindale <joel.chippindale@gmail.com>
2009-10-20 07:08:53 +0100

    Upgrade to Cucumber 0.4.2
    Main change is removal of the custom screenshot format. This is replaced by using
    @screenshot tags for features/scenarios to be captured.

---
+++ lib/cucumber_screenshot/world.rb
@@ -0,0 +1,60 @@
+module CucumberScreenshot
+  module World
+
+    attr_accessor :response_body_for_last_screenshot, :screenshot_index, :screenshot_directory, :current_feature_segment, :current_scenario_segment
+
+    def base_screenshot_directory_name
+      # TODO: Make this configurable
+      # TODO: Make this work for other frameworks e.g. sinatra
+      "#{RAILS_ROOT}/features/screenshots"
+    end
+
+    def screenshot(directory_name = base_screenshot_directory_name, file_name = "screenshot-#{Time.now.to_i}")
+      File.makedirs("#{directory_name}/html")
+
+      html_file_name = "#{directory_name}/html/#{file_name}.html"
+      File.open(html_file_name, "w") do |f|
+        f.write rewrite_javascript_and_css_and_image_references(current_response_body)
+      end
+
+      command = "snapurl file://#{html_file_name} --no-thumbnail --no-clip --filename #{file_name} --output-dir #{directory_name}"
+      `#{command}`
+      if $? == 0
+        announce "- Screenshot saved to #{directory_name}/#{file_name}.png\n"
+        self.response_body_for_last_screenshot = current_response_body
+        true
+      else
+        report_error_running_screenshot_command(command)
+        false
+      end
+    end
+
+    def screenshot_due?
+      current_response_body && current_response_body != response_body_for_last_screenshot && !webrat_session.redirect?
+    end
+
+    protected
+      def current_response_body
+        webrat_session.send(:response) && webrat_session.response_body
+      end
+
+      def rewrite_javascript_and_css_and_image_references(response_html) # :nodoc:
+        doc_root = webrat_session.adapter.doc_root
+        return response_html unless doc_root
+        response_html.gsub(/"\/(javascripts|stylesheets|images)\//, '"' + doc_root + '/\1/')
+      end
+
+      def report_error_running_screenshot_command(command)
+        STDERR.puts "
+Unable to make screenshot, to find out what went wrong try the following from the command line
+
+    #{command}
+
+Please remember need to have installed the gem snapurl to take screenshots
+
+    gem install snapurl
+
+"
+      end
+  end
+end
---
+++ spec/cucumber_screenshot/world_spec.rb
@@ -0,0 +1,51 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+class TestWorld
+  include CucumberScreenshot::World
+end
+
+describe CucumberScreenshot::World do
+  before(:each) do
+    @session = TestWorld.new
+  end
+
+  describe '#base_screenshot_directory_name' do
+    it "add features/screenshots to rails root" do
+      ::RAILS_ROOT = 'tmp'
+      TestWorld.new.base_screenshot_directory_name.should == 'tmp/features/screenshots'
+    end
+  end
+
+  describe '#screenshot' do
+    before(:each) do
+      File.stub!(:makedirs => true, :open => true)
+      @session.stub!(:base_screenshot_directory_name => 'tmp/features/screenshots', :response_body => 'foo')
+      @session.stub!(:'`' => 'foo')
+      # While $? is not being set by the backtick stub
+      @session.stub!(:report_error_running_screenshot_command => true)
+    end
+
+    it 'should call File.makedirs' do
+      File.should_receive(:makedirs).with('1/2/html').and_return(true)
+      @session.screenshot('1/2', 'snapshot-001')
+    end
+  end
+
+  describe 'protected' do
+    describe '#rewrite_javascript_and_css_and_image_references' do
+      before(:each) do
+        @session.stub!(:webrat_session => stub('webrat_session', :adapter => stub('rails_adapter', :doc_root => '/tmp/public')))
+      end
+
+      it 'should replace relative /javascripts/ references with file references' do
+        @session.send(:rewrite_javascript_and_css_and_image_references, '<script src="/javascripts/application.js?1255077419" type="text/javascript"></script>').
+          should == '<script src="/tmp/public/javascripts/application.js?1255077419" type="text/javascript"></script>'
+      end
+
+      it 'should replace relative /javascripts/ references with file references' do
+        @session.send(:rewrite_javascript_and_css_and_image_references, '<link href="/stylesheets/application.css" media="screen" rel="stylesheet" type="text/css" />').
+          should == '<link href="/tmp/public/stylesheets/application.css" media="screen" rel="stylesheet" type="text/css" />'
+      end
+    end
+  end
+end
